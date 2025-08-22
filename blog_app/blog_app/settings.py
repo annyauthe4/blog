7d9c64387fe619bb0d8c3519ea50ab270b8aadf2
Otetumo Oluwaseun <annyauthe@yahoo.com>
2025-08-22 07:02:49 +0100

    Update actions

--- blog_app/blog_app/settings.py
+++ blog_app/blog_app/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -116,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
