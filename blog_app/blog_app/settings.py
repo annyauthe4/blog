d8d6695bf7a8a8dbe666f5282fcd6db43fe4a586
Otetumo Oluwaseun <annyauthe@yahoo.com>
2025-11-03 21:57:16 +0100

    Include external url for db

--- blog_app/blog_app/settings.py
+++ blog_app/blog_app/settings.py
@@ -88,18 +88,26 @@ WSGI_APPLICATION = 'blog_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+INTERNAL_DB_URL = config("INTERNAL_DB_URL", default="")
+EXTERNAL_DB_URL = config("EXTERNAL_DB_URL", default="")
+
+if INTERNAL_DB_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(INTERNAL_DB_URL, conn_max_age=600)
     }
-}
 
-DATABASE_URL = config("INTERNAL_DB_URL", default="")
+elif EXTERNAL_DB_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(EXTERNAL_DB_URL, conn_max_age=600, ssl_require=True)
+    }
 
-if DATABASE_URL:
-    DATABASES["default"] = dj_database_url.parse(DATABASE_URL, conn_max_age=600, ssl_require=True)
-
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
